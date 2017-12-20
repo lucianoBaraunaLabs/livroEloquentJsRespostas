ce6d517db3b9bffde88c0a92ef0e8f2c1c062cc2
Luciano Baraúna <baraunaluciano@gmail.com>
2017-12-20 13:02:58 -0200

    adicionando solução do livro respCap2-1.2.js

--- respCap2-1.2.js
+++ respCap2-1.2.js
@@ -7,21 +7,33 @@ function fizzBuzz () {
     //   console.log('Fizz ==> ' , i)
     // } else if (i % 5 === 0) {
     //   console.log('Buzz ==> ' , i)
-    // }
+    // } else {
+//         console.log('Número ==> ' , i)
+//       }
 
     //  Solução 2 funcionando
-    if(i % 3 === 0 || i % 5 === 0) {
-      if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz ==> ' , i)
-      } else if (i % 3 === 0) {
-          console.log('Fizz ==> ' , i)
-      } else if(i % 5 === 0) {
-        console.log('Buzz ==> ' , i)
-      }
-    } else {
-      console.log('Número ==> ' , i)
-    }
+//     if(i % 3 === 0 || i % 5 === 0) {
+//       if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz ==> ' , i)
+//       } else if (i % 3 === 0) {
+//           console.log('Fizz ==> ' , i)
+//       } else if(i % 5 === 0) {
+//         console.log('Buzz ==> ' , i)
+//       }
+//     } else {
+//       console.log('Número ==> ' , i)
+//     }
     
+//   }
+  
+  // Solução do livro
+  for (var n = 1; n <= 100; n++) {
+    var output = "";
+    if (n % 3 == 0)
+      output += "Fizz";
+    if (n % 5 == 0)
+      output += "Buzz";
+    console.log(output || n);
   }
     
 }
