841ace0fe64da4b471e5992932eb2e16eb7f9903
Luciano Baraúna <baraunaluciano@gmail.com>
2017-12-20 12:56:38 -0200

    Update respCap2-1.2.js

--- respCap2-1.2.js
+++ respCap2-1.2.js
@@ -1,21 +1,25 @@
 function fizzBuzz () {
-  for(let i = 0; i <= 20; i++) {
-    // if(i % 3 === 0) {
-    //   console.log('Fizz ==> ' , i)
-    // } else if( i % 5 === 0 ) {
-    //   console.log('Buzz ==> ' , i)
-    // } else if( i % 3 === 0 && i % 5 === 0 ) {
+  for(let i = 0; i <= 100; i++) {
+    //  Solução 1 funcionando
+    // if (i % 3 === 0 && i % 5 === 0) {
     //   console.log('FizzBuzz ==> ' , i)
-    // } else {
-    //   console.log('não é divisível por 3 e nem 5 ==> ', i )  
+    // } else if (i % 3 === 0) {
+    //   console.log('Fizz ==> ' , i)
+    // } else if (i % 5 === 0) {
+    //   console.log('Buzz ==> ' , i)
     // }
 
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz ==> ' , i)
-    } else if (i % 3 === 0) {
-      console.log('Fizz ==> ' , i)
-    } else if (i % 5 === 0) {
-      console.log('Buzz ==> ' , i)
+    //  Solução 2 funcionando
+    if(i % 3 === 0 || i % 5 === 0) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz ==> ' , i)
+      } else if (i % 3 === 0) {
+          console.log('Fizz ==> ' , i)
+      } else if(i % 5 === 0) {
+        console.log('Buzz ==> ' , i)
+      }
+    } else {
+      console.log('Número ==> ' , i)
     }
     
   }
