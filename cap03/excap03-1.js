c97e984c21bb2500c818fc71c103fa8f0479efc7
Luciano Baraúna <baraunaluciano@gmail.com>
2018-09-18 14:57:23 -0300

    add editorconfig e respondendo ex cap03 1

---
+++ cap03/excap03-1.js
@@ -0,0 +1,19 @@
+// Solução do livro
+// function min(a, b) {
+//     if (a < b)
+//       return a;
+//     else
+//       return b;
+// }
+
+
+// Minha solução
+function min(num1, num2) {
+    if (num1 < num2) return num1
+    return num2
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
